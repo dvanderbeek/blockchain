1461f8db2b8665587c3e1f64bd8580c38e2719d0
David Van Der Beek <earlynovrock@gmail.com>
2025-07-24 14:54:50 -0700

    use archive nodes for near

--- app/protocols/blockchain/near.rb
+++ app/protocols/blockchain/near.rb
@@ -4,11 +4,11 @@ module Blockchain::Near
   def networks
     {
       mainnet: {
-        rpc_url: 'https://rpc.mainnet.near.org',
+        rpc_url: 'https://archival-rpc.mainnet.near.org',
         explorer_url: 'https://nearblocks.io'
       },
       testnet: {
-        rpc_url: 'https://rpc.testnet.near.org',
+        rpc_url: 'https://archival-rpc.testnet.near.org',
         explorer_url: 'https://testnet.nearblocks.io'
       }
     }
