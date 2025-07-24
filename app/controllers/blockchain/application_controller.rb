09a3efb3c011cb9adb543500649b62a1c087a9da
David Van Der Beek <earlynovrock@gmail.com>
2025-07-23 20:11:18 -0700

    bypass auth

--- app/controllers/blockchain/application_controller.rb
+++ app/controllers/blockchain/application_controller.rb
@@ -1,5 +1,6 @@
 module Blockchain
   class ApplicationController < ::ApplicationController
-            layout 'layouts/application' # Use the parent app's layout
+    layout 'layouts/application' # Use the parent app's layout
+    allow_unauthenticated_access if respond_to?(:allow_unauthenticated_access)
   end
 end
